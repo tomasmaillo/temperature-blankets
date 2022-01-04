b10201d35c395d7a917b070b27005809cc5848e9
tomas <tomasroma64@gmail.com>
2022-01-04 16:47:42 +0000

    add text

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <p class="bottom-middle">
+      <a href="https://mystifying-lumiere-22fd64.netlify.app/">
+        temperature-blankets
+      </a>
+      <span> | </span>
+      <a href="https://github.com/Tomasroma64/temperature-blankets">GitHub</a>
+      <span> feel free to submit a pull request | by </span>
+      <a href="http://tomasmaillo.com">tomasmaillo</a>
+    </p>
+  );
+}
+
+export default Footer;
---
+++ src/GUI.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./GUI.css";
+
+function GUI() {
+  return (
+    <>
+      <div>
+        <h1>Temperature Blankets</h1>
+        <h3>Tap to starts</h3>
+      </div>
+    </>
+  );
+}
+
+export default GUI;
